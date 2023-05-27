355db0ac1cd579fe3060663a0277f0ab35ed031e
chung won <pcw1405@naver.com>
2023-05-27 13:02:03 +0900

    member컨트롤러가 메인컨트롤러하고 중복되어서 변경 login -> loginForm

--- src/main/java/com/netf/netflix/Controller/MemberController.java
+++ src/main/java/com/netf/netflix/Controller/MemberController.java
@@ -3,6 +3,7 @@ package com.netf.netflix.Controller;
 import com.netf.netflix.Dto.MemberDto;
 import com.netf.netflix.Entity.Member;
 import com.netf.netflix.Service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,8 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class MemberController {
     private MemberService memberService;
 
-    @GetMapping("/login")
-    public String loginMember(){
+    @GetMapping("/loginForm")
+    public String loginFormMember(){
         return "/login";
     }
 
