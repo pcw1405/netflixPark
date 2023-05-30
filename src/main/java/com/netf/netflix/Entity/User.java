dae03629697874463603a275a0d504180911979a
0minyun <yym0320@naver.com>
2023-05-30 12:24:44 +0900

    디테일작업시작.

--- src/main/java/com/netf/netflix/Entity/User.java
+++ src/main/java/com/netf/netflix/Entity/User.java
@@ -26,9 +26,9 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "member_id") // "member_id"는 Member 엔티티에서 Member 엔티티의 ID와 매핑된 필드의 이름입니다.
     private Member member;
-
-    @OneToOne
-    private long profile_img;
+//
+//    @OneToOne
+//    private long profile_img;
 
     @Override
     public String toString() {
