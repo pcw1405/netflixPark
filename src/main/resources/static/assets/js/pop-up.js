1915e0fd9b06c68eb847aebfbd63d33481b160ec
pcw1405 <pcw14057@gmail.com>
2023-06-14 18:50:54 +0900

    일단 팝업 css 수정 및 폰트 적용 지금까지 한 것 push

--- src/main/resources/static/assets/js/pop-up.js
+++ src/main/resources/static/assets/js/pop-up.js
@@ -32,19 +32,19 @@ $(document).ready(function () {
 
         var $video = $('<video>', {
             src: videoUrl,
-           controls: true, // 컨트롤러 비활성화
+           controls: false, // 컨트롤러 비활성화
              autoplay: true, // 자동 재생 활성화
 //             muted: false, // 음소거 비활성화 (선택적)
-            width: '495px',
-            height: '400px',
+            width: '100%',
+            height: '100%',
             class:"popupVideo"
         });
 
         $video.prop("autoplay", true);
         $videoSpace.empty().append($video);
         $videoSpace.css({
-            width: "500px",
-            height: "400px"
+            width: "100%",
+            height: "100%"
         });
 
         var $textContainer = $(".textContainer");
@@ -56,10 +56,10 @@ $(document).ready(function () {
                 height: "250px"
                 }
             });
-            $textContainer.empty().append($textSpace);
+//            $textContainer.empty().append($textSpace);
         }
 
-        $textSpace.empty().append("<hr>");
+//        $textSpace.empty().append("<hr>");
 
         var $descriptionText = $("<div>");
 
@@ -81,14 +81,30 @@ $(document).ready(function () {
  $('.popup').find('.saveLike i').attr('data-iColor', videoIColor);
 
         $descriptionText.empty().append(
-          $("<div>").text("제목: " + videoNm),
-          $("<div>").text("장르: " + videoGenres),
-          $("<div>").text("설명: " + videoDescription),
-          $("<div>").text("배우: " + videoActors),
-          $("<div>").text("감독: " + videoCast)
+//          $("<div>").text("제목: " + videoNm),
+//          $("<div>").text("장르: " + videoGenres),
+          $("<span>").text("설명: " + videoDescription),
+//          $("<div>").text("배우: " + videoActors)
+//          $("<div>").text("감독: " + videoCast)
         );
 
-        $textSpace.empty().append($descriptionText);
+  var $descriptionText2 = $("<div>");
+        $descriptionText2.empty().append(
+//                  $("<div>").text("제목: " + videoNm),
+                  $("<span>").text("장르: " + videoGenres),
+        //          $("<div>").text("설명: " + videoDescription),
+                  $("<span>").text("배우: " + videoActors),
+                  $("<span>").text("감독: " + videoCast)
+                );
+
+                $('.titleV').empty().append( $("<span>").text( videoNm));
+            $('.story').empty().append($descriptionText);
+
+          $('.ganre').empty().append( $("<span>").text( videoCast));
+            $('.actor').empty().append( $("<span>").text( videoActors));
+             $('.videoCast').empty().append( $("<span>").text( videoCast));
+
+//        $('.story_box').empty().append($descriptionText);
 
               $('.saveLike i').each(function() {
                     var iColor = $(this).attr('data-iColor');
